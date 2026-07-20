9f0374eda1c7b66d5453ddd1f6c4e5dd47432b59
amirhmd <opensource.amirhmd@gmail.com>
2026-07-20 14:48:12 +0200

    polish docker start

--- local-dev/local_dev_start.sh
+++ local-dev/local_dev_start.sh
@@ -6,6 +6,4 @@ cd "$(dirname "$0")"
 echo "Starting local dev environment..."
 docker compose up -d
 
-echo ""
-echo "Postgres running:"
-docker ps --filter "name=configsync-postgres"
+echo "Happy Coding!"
