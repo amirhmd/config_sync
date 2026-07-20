6c111cd29fa6e7415753b23be1da296965f5951d
amirhmd <opensource.amirhmd@gmail.com>
2026-07-20 12:59:40 +0200

    add annotation

--- src/ConfigSync/Program.cs
+++ src/ConfigSync/Program.cs
@@ -1,16 +1,16 @@
+using JetBrains.Annotations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
 
 namespace ConfigSync;
 
+[UsedImplicitly]
 public class Program
 {
     public static void Main(string[] args)
     {
         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
         WebApplication app = builder.Build();
-
         app.Run();
     }
 }
